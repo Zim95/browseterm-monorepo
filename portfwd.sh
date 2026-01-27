bcf20d2b9caed9e237b28da28cf9c6eca3350019
Zim95 <shresthanamah@gmail.com>
2026-01-28 00:51:22 +0530

    Updated all submodules to their latest commits

--- portfwd.sh
+++ portfwd.sh
@@ -3,7 +3,7 @@
 # Terminal 1 - browseterm-server
 kubectl port-forward -n browseterm-new --address 192.168.0.3 svc/browseterm-server-development-service 9999:9999 &
 
-# Terminal 2 - socket-ssh  
+# Terminal 2 - socket-ssh (with debug port 9229)
 kubectl port-forward -n browseterm-new --address 192.168.0.4 svc/socket-ssh-development-service 8000:8000 &
 
 wait
